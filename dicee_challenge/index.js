7785a5382e7ea20af6b8ccc05aa28b41a1fbc9c6
Pareekshit08 <105261006+Pareekshit08@users.noreply.github.com>
2024-05-11 13:22:13 +0530

    Add files via upload

---
+++ dicee_challenge/index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+document.getElementById("img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+var winner = (randomNumber1 == randomNumber2) ? -1 : (randomNumber1 > randomNumber2 ? 1 : 2);
+var winnerText = "";
+if (winner == -1) {
+    winnerText = "Draw!";
+} else {
+  winnerText = "Player " + winner + " Wins!";
+  winnerText = (winner==1)?"🚩 " + winnerText : winnerText += " 🚩";
+  // if (winner == 1) {
+  //   winnerText = "🚩 " + winnerText;
+  // } else {
+  //   winnerText += " 🚩";
+  // }
+}
+document.getElementsByTagName("h1")[0].innerHTML = winnerText;
